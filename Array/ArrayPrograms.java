e6bc98cc39d66d9e751490a0bc7758f7abb3a3a3
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-18 00:02:20 +0530

    array initialization and declaration

--- Array/ArrayPrograms.java
+++ Array/ArrayPrograms.java
@@ -3,5 +3,14 @@ package Array;
 class ArrayPrograms {
   public static void main(String[] args) {
 
+    int[] arr = new int[10];
+
+    int n = 10;
+    int[] arr1 = new int[n];
+
+    int[] arr2 = new int[] { 10, 15, 30, 50 };
+    for (int i : arr2) {
+      System.out.println(i);
+    }
   }
 }
