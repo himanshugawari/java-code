7b8f07f5b7dcc0c16ea3ac3c86b9682a0f585acc
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-18 00:07:28 +0530

    array search

--- Array/ArrayPrograms.java
+++ Array/ArrayPrograms.java
@@ -3,14 +3,27 @@ package Array;
 class ArrayPrograms {
   public static void main(String[] args) {
 
-    int[] arr = new int[10];
+    // int[] arr = new int[10];
+    // int n = 10;
+    // int[] arr1 = new int[n];
+    // int[] arr2 = new int[] { 10, 15, 30, 50 };
+    // for (int i : arr2) {
+    // System.out.println(i);
+    // }
 
-    int n = 10;
-    int[] arr1 = new int[n];
+    int[] temp = new int[] { 20, 5, 7, 25 };
+    System.out.println(Search(temp, 250));
 
-    int[] arr2 = new int[] { 10, 15, 30, 50 };
-    for (int i : arr2) {
-      System.out.println(i);
-    }
   }
+
+  public static int Search(int[] arr, int x) {
+    int n = arr.length;
+    for (int i = 0; i < n; i++) {
+      if (arr[i] == x) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
 }
