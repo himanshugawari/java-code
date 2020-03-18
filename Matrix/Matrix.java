2b4ed7a52d8030337a1f15ee218508773a872999
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-18 17:13:41 +0530

    Jagged Array

--- Matrix/Matrix.java
+++ Matrix/Matrix.java
@@ -3,9 +3,28 @@ package Matrix;
 class Matrix {
   public static void main(String[] args) {
     // int[][] arr = new int[][] { { 1, 2, 3 }, { 4, 5, 6 } };
-    int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
-    print2dArray(arr);
+    // int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
+    // print2dArray(arr);
 
+    // int[][] temp = new int[2][3];
+    // print2dArray(temp);
+
+    // Jagged Arrays
+    int m = 3;
+    int[][] a = new int[m][];
+    print2dJaggedArray(a);
+
+  }
+
+  public static void print2dJaggedArray(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = new int[i + 1];
+      for (int j = 0; j < arr[i].length; j++) {
+        arr[i][j] = i;
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void print2dArray(int[][] arr) {
