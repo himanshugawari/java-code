b5750c0e9ee968afd44c808c5e82b553abfc512b
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-18 14:50:53 +0530

    added search and insert into an array

--- Array/ArrayPrograms.java
+++ Array/ArrayPrograms.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 class ArrayPrograms {
   public static void main(String[] args) {
 
@@ -11,9 +13,39 @@ class ArrayPrograms {
     // System.out.println(i);
     // }
 
-    int[] temp = new int[] { 20, 5, 7, 25 };
-    System.out.println(Search(temp, 250));
+    // Search
+    // int[] temp = new int[] { 20, 5, 7, 25 };
+    // System.out.println(Search(temp, 250));
 
+    // Insert an elemnet at a position
+    // int[] temp = new int[5];
+    // Arrays.fill(temp, -1);
+    // temp[0] = 5;
+    // temp[1] = 7;
+    // Insert(temp, temp.length - 1, 3, 2);
+    // Insert(temp, temp.length - 1, 10, 2);
+    // for (int i = 0; i < temp.length; i++) {
+    // System.out.print(temp[i] + " ");
+    // }
+
+  }
+
+  public static int Insert(int[] arr, int n, int x, int position) {
+    int capacity = -1;
+    for (int i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] != -1) {
+        capacity = i + 1;
+      }
+    }
+    if (n == capacity) {
+      return n;
+    }
+    int index = position - 1;
+    for (int i = n - 1; i >= index; i--) {
+      arr[i + 1] = arr[i];
+    }
+    arr[index] = x;
+    return n + 1;
   }
 
   public static int Search(int[] arr, int x) {
