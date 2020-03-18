98ab9aa912af5934c4f0a640c1ace6b941601a2f
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-18 17:24:40 +0530

    print in snake pattern

--- Matrix/Matrix.java
+++ Matrix/Matrix.java
@@ -9,11 +9,29 @@ class Matrix {
     // int[][] temp = new int[2][3];
     // print2dArray(temp);
 
-    // Jagged Arrays
-    int m = 3;
-    int[][] a = new int[m][];
-    print2dJaggedArray(a);
+    // Jagged Arrays (rows are of different sizes)
+    // int m = 3;
+    // int[][] a = new int[m][];
+    // print2dJaggedArray(a);
 
+    // Snake Pattern
+    int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+    printMatrixInSnakePattern(arr);
+
+  }
+
+  public static void printMatrixInSnakePattern(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      if (i % 2 == 0) {
+        for (int j = 0; j < arr[i].length; j++) {
+          System.out.print(arr[i][j] + " ");
+        }
+      } else {
+        for (int j = arr[i].length - 1; j >= 0; j--) {
+          System.out.print(arr[i][j] + " ");
+        }
+      }
+    }
   }
 
   public static void print2dJaggedArray(int[][] arr) {
