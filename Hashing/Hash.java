8aaf4c5d457c014d0e08e2582eedfd6e1698b2ee
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-19 10:44:12 +0530

    Hashmap

--- Hashing/Hash.java
+++ Hashing/Hash.java
@@ -1,8 +1,11 @@
 package Hashing;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
 public class Hash {
   public static void main(String[] args) {
@@ -21,26 +24,48 @@ public class Hash {
     // m.remove(22);
     // System.out.println(m.search(22));
 
-    Set<String> h = new HashSet<>();
-    h.add("gfg");
-    h.add("himanshu");
-    h.add("hardik");
+    // HashSet
+    // Set<String> h = new HashSet<>();
+    // h.add("gfg");
+    // h.add("himanshu");
+    // h.add("hardik");
+    // System.out.println(h);
+    // h.remove("gfg");
+    // System.out.println(h);
+    // System.out.println(h.contains("hardik"));
+    // Iterator<String> i = h.iterator();
+    // while (i.hasNext()) {
+    // System.out.print(i.next() + " ");
+    // }
+    // System.out.println(h.size());
+    // for (String s : h) {
+    // System.out.print(s + " ");
+    // }
 
-    System.out.println(h);
-    h.remove("gfg");
-    System.out.println(h);
-    System.out.println(h.contains("hardik"));
-
-    Iterator<String> i = h.iterator();
-    while (i.hasNext()) {
-      System.out.print(i.next() + " ");
+    // HashMap
+    Map<String, Integer> m = new HashMap<>();
+    m.put("himanshu", 10);
+    m.put("hardik", 20);
+    m.put("rohidas", 30);
+    System.out.println(m);
+    System.out.println(m.size());
+    for (Entry<String, Integer> e : m.entrySet()) {
+      System.out.println(e.getKey() + " " + e.getValue());
     }
-
-    System.out.println(h.size());
-
-    for (String s : h) {
-      System.out.print(s + " ");
+    if (m.containsKey("rohidas")) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
     }
+    m.remove("himanshu");
+    System.out.println(m.size());
+    if (m.containsValue(30)) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+    System.out.println(m.get("hardik"));
+    System.out.println(m.get("himanshu"));
 
   }
 }
