98521b262a8b1fff9dc9059811f3a1fd515077e8
Himanshu Gawari <himanshu25031991@gmail.com>
2020-03-19 11:01:53 +0530

    string

---
+++ String/StringProg.java
@@ -0,0 +1,12 @@
+package String;
+
+public class StringProg {
+
+  public static void main(String[] args) {
+    String s = "geeksforgeeks";
+    System.out.println(s.indexOf("geeks"));
+    System.out.println(s.indexOf("for"));
+    System.out.println(s.substring(1, 3));
+
+  }
+}
